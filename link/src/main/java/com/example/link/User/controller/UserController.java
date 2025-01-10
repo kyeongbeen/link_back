4357a03e48ea7cdb431ecc34b8207faa39ddf3d1
KyeongBin <kyeongbin0205@naver.com>
2025-01-10 11:20:14 +0900

    chore: api 설명
    /login api는 body 가 아닌 form data 형식으로 보내야하기 때문에 확인해야함

--- link/src/main/java/com/example/link/User/controller/UserController.java
+++ link/src/main/java/com/example/link/User/controller/UserController.java
@@ -6,6 +6,8 @@ import com.example.link.User.dto.SignupDTO;
 import com.example.link.User.dto.UserDTO;
 import com.example.link.User.dto.UserListDTO;
 import com.example.link.User.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,26 +23,33 @@ import java.util.List;
 @Slf4j
 @RequestMapping
 @RequiredArgsConstructor
+@Tag(name = "User", description = "유저 API")
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/user/lists")
+    @Operation(summary = "프로젝트에 있는 사용자 전원 조회")
     public ResponseEntity<List<UserListDTO>> getUserLists(@RequestParam int projectId) {
         return new ResponseEntity<>(userService.getUserLists(projectId), HttpStatus.OK);
     }
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입")
     public ResponseEntity<UserDTO> signup(@RequestBody SignupDTO signupDTO) {
         return new ResponseEntity<>(userService.signup(signupDTO), HttpStatus.OK);
     }
 
     @PatchMapping("/logout")
+    @Operation(summary = "로그아웃")
     public ResponseEntity<String> logout(@RequestParam String email) {
         return new ResponseEntity<>(userService.logout(email), HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인",
+            description = "requestbody가 아닌 form data 형태로 데이터 전송해야함\n" +
+                    "\nusername 에는 email, password 에는 해당유저의 password 입력")
     public void login(@RequestBody LoginDTO loginDTO) {}
 
 
